afad84400d80e2e9cc760bbbeb8da254729e2580
moukaddam <mhd.moukaddam@gmail.com>
2014-10-21 16:26:41 -0700

    first commit

---
+++ dcp2root.cxx
@@ -0,0 +1,155 @@
+// edited to include monitor - 29/09/09
+// #########################################################################################3
+// Compillation in /home/tibor/12C/DcptoROOT folder
+//   (a) edit libDAQ.cxx if needed
+//   (b) run root
+//   (c) compilation in ROOT: .L libDAQ/libDAQ.cxx++
+//   (d) .x dcp2root.cxx
+//   (e) provide filename when prompted
+
+#include <iostream.h>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+void dcp2root(){
+  //  PrepareConfig();
+  //  PrepareTree();
+
+  // Set name for used input and output files
+  
+  cout <<"========================================"<<endl;
+  cout <<"DCP to ROOT file conversion             "<<endl;
+  cout <<"========================================"<<endl;
+  cout <<"STAGE 1: Enter DCP file for conversion: ";
+  string infileName="output.root";
+  getline( cin, infileName);
+
+  string outfileName="event.root";
+  outfileName.assign(infileName);
+  outfileName.append(".root");
+
+  TFile *file = new TFile(outfileName.c_str(),"recreate");
+
+  LoadDCPFile(infileName.c_str());
+  file->Write();
+} 
+
+// Creates the TDAQConfig object which specifies how the raw DAQ
+// channels should be loaded into the tree.
+TDAQConfig *PrepareConfig(void){
+  // Create the object.
+  TDAQConfig *RetVal = new TDAQConfig();
+
+  // Creates a new branch. In ROOT, the events are stored in trees, which have
+  // branches and leaves. Leaves contain the actual event data, and branches
+  // group the leaves together. This allows for faster processing of events, as
+  // branches can be disabled (resulting in less data being loaded of the disk
+  // and processed for each event).
+  TDAQBranch *CurBranch = new TDAQBranch("Miel", "Miel");
+
+  // This adds a new ADC to the branch. The parameters for the TDAQADC
+  // constructor are (<interface>, <ADC number>, <name>, <description>, <number
+  // of channels>). In detail:
+  // [] Interface: The interface group from the experiment definition.  Note
+  //    that it's zero based, so "Coincidence Interface 1" is put in as
+  //    interface 0.
+  // [] ADC number: Like the interface, it's from the experiment definition and
+  //    zero based. So ADC 14.1 is interface 13 ADC 0.
+  // [] Name: The name of the leaf to store the ADC data in. Does not have to
+  //    match experiment definition or anything.
+  // [] Description: A description of the leaf. Useful if at some point you
+  //    forget what the leaf is.
+  // [] Number of channels: Name says it all really.
+  //CurBranch->GetADCs()->Add(new TDAQADC(0, 0, "DE", "DeltaE", 2048));
+  //CurBranch->GetADCs()->Add(new TDAQADC(0, 1, "E", "Energy", 2048));
+  
+  // Pair spectrometer runs: Jul-2013
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 0, "Miel1E",    "Miel1E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 1, "Miel2E",    "Miel2E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 2, "Miel3E",    "Miel3E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 3, "Miel4E",    "Miel4E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 4, "Miel5E",    "Miel5E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 5, "Miel6E",    "Miel6E", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 6, "Aptherix",  "Aptherix", 8192));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 7, "HallProbe", "HallProbe", 2048));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 8, "VcontE",    "VcontE", 2048));
+   CurBranch->GetADCs()->Add(new TDAQADC(0, 9, "VcontG",    "VcontG", 2048));
+
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 0, "Miel1T", "Miel1T", 4096));
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 1, "Miel2T", "Miel2T", 4096));
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 2, "Miel3T", "Miel3T", 4096));
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 3, "Miel4T", "Miel4T", 4096));
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 4, "Miel5T", "Miel5T", 4096));
+   CurBranch->GetADCs()->Add(new TDAQADC(5, 5, "Miel6T", "Miel6T", 4096));
+
+  // This now adds the branch to the DAQConfig object.
+  RetVal->GetBranches()->Add(CurBranch);
+
+  // Another branch. Note that here, we only have a single leaf. As a result,
+  // the leaf is ignored and ROOT treats the branch "ToF" as a leaf.
+  //CurBranch = new TDAQBranch("Expt", "Expt");
+   //  CurBranch->GetADCs()->Add(new TDAQADC(0, 0, "MWPCToF", "Multiwire time-of-flight", 1024));
+   //CurBranch->GetADCs()->Add(new TDAQADC(0, 8,  "FissionPulser", "FissionPulser", 1024));
+   //CurBranch->GetADCs()->Add(new TDAQADC(0, 9,  "Monitor1", "Monitor1", 1024));
+   //CurBranch->GetADCs()->Add(new TDAQADC(0, 10, "Monitor2", "Monitor2", 1024));
+   //RetVal->GetBranches()->Add(CurBranch);
+
+
+   
+   //   CurBranch = new TDAQBranch("Alpha", "Alpha");
+   //   CurBranch->GetADCs()->Add(new TDAQADC(0, 11, "EAnnular", "Energy Annular", 1024));
+   //   CurBranch->GetADCs()->Add(new TDAQADC(0, 12, "TAnnular", "Time Annular", 1024));
+   //   CurBranch->GetADCs()->Add(new TDAQADC(0, 13, "ESBFront",  "Energy SurfaceBarrier front", 1024));
+   //   RetVal->GetBranches()->Add(CurBranch);
+
+//   CurBranch = new TDAQBranch("Annular", "Annular");
+//   CurBranch->GetADCs()->Add(new TDAQADC(0, 12, "XPos", "X positon", 1024));
+//   CurBranch->GetADCs()->Add(new TDAQADC(0, 13, "YPos", "Y position", 1024));
+//   RetVal->GetBranches()->Add(CurBranch);
+
+//   CurBranch = new TDAQBranch("Front", "Front");
+//   CurBranch->GetADCs()->Add(new TDAQADC(0, 14, "Mon1", "Monitor 1", 1024));
+//   CurBranch->GetADCs()->Add(new TDAQADC(0, 15, "Mon2", "Monitor 2", 1024));
+//   RetVal->GetBranches()->Add(CurBranch);
+
+  // Everything is added to the config, so we're all done.
+  return RetVal;
+}
+
+// Creates a new tree, based on the configuration above.
+TTree *PrepareTree(void)
+{
+  // Create the configuration using the function above.
+  TDAQConfig *DAQConfig = PrepareConfig();
+  // Create the new tree. You can set the name and description to something more
+  // appropriate for your experiment.
+  TTree *MielTree = new TTree("MielTree", "Miel event tree");
+  // This calls into libDAQ to have the TDAQConfig object set up the tree. This
+  // creates all the branches, etc.
+  DAQConfig->InitTree(MielTree);
+  // We no longer need the DAQConfig object, so delete it so as to not leak
+  // memory.
+  delete DAQConfig;
+  // And we're all done.
+  return MielTree;
+}
+
+// Creates a new TTree object and loads a DCP file into the tree.
+TTree *LoadDCPFile(char *Filename)
+{
+  // Like the function above, we create the configuration and set up the tree.
+  TDAQConfig *DAQConfig = PrepareConfig();
+  TTree *MielTree = new TTree("MielTree", "Miel event tree");
+  DAQConfig->InitTree(MielTree);
+  // Now, we need to create another object that does the actual loading. This
+  // object is passed the DAQConfig object so it knows how to treat the input
+  // DCP file and how to store it in the tree.
+  TDCPReader Reader(DAQConfig);
+  // This does the actual DCP->TTree load.
+  Reader.FillTreeFromDCPFile(MielTree, Filename);
+  // Like above, tidy up and return.
+  delete DAQConfig;
+  return MielTree;
+}
---
+++ struct2class.cxx
@@ -0,0 +1,278 @@
+// #########################################################################################
+// compile :
+//  g++ struct2class.cxx libAnalysis/libMielData.so\
+                -IlibAnalysis --std=c++0x -o struct2class\
+                -O2 `root-config --cflags --libs`\
+                -lTreePlayer -lgsl -lgslcblas 
+// #########################################################################################
+
+
+//c++
+#include <stdio.h> //c
+#include <stdlib.h> //c
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//Root 
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+
+//User 
+#include "./libAnalysis/TMielData.h"
+
+
+
+// #########################################################################################
+//                               global variables 
+// #########################################################################################
+
+//Declare global variables
+
+//structure to read the basic tree
+// *************************************************************************************
+// I M P O R T A N T  : The type (uShort_t) and the order of the declaration is relevant
+// Use : MielTree->Print() to print the structure
+//
+//*Br    0 :Miel      : Miel1E/s:Miel2E:Miel3E:Miel4E:Miel5E:Miel6E:Aptherix:  *
+//*         | HallProbe:VcontE:VcontG:Miel1T:Miel2T:Miel3T:Miel4T:Miel5T:Miel6T*
+//
+// **************************************************************
+struct Miel_st {
+ UShort_t Miel1E;  UShort_t Miel2E; UShort_t Miel3E; UShort_t Miel4E; UShort_t Miel5E; UShort_t Miel6E;  
+ UShort_t Aptherix; UShort_t HallProbe; UShort_t VcontE; UShort_t VcontG;
+ UShort_t Miel1T;  UShort_t Miel2T; UShort_t Miel3T; UShort_t Miel4T; UShort_t Miel5T; UShort_t Miel6T; 
+ } gMiel_st; // old tree , struct based
+
+TMielData* gMielData; // new tree, vector based
+TTree *gNewTree ;
+TTree *gOldTree ;
+TFile *gInputFile; 
+TFile *gOutputFile;
+
+//Buffer variables
+vector <UShort_t>	gBuffer_energy; 
+vector <UShort_t>	gBuffer_time; 
+UShort_t gBuffer_gamma ; 
+UShort_t gBuffer_hall ; 
+UShort_t gBuffer_VContE ;  
+UShort_t gBuffer_VContG ; 	
+
+//Declare functions
+void GetEvent(int i); 
+void ResetBuffers(); 
+float CalibrateEnergy(int segment, int E_charge); 
+float CalibrateTime(int segment, int T_charge); 
+void PrintBuffers();
+void PrintMiel();
+
+// #########################################################################################
+//                                Main function
+// #########################################################################################
+
+int main(int argc, char **argv){
+   
+ if(argc < 2)	{
+printf(" use : ./struct2class XXX.root (YYY.root .. etc...) \n");
+return 1;
+}
+
+bool GoodEvent=false; 
+gMielData = new TMielData();
+
+// Iterate through the input files
+for(int i=1;i<argc;i++)	{
+
+	printf(" Reading file %s\t", argv[i]);
+	string outputname = "Data_";
+	string inputname = argv[i];
+	outputname.append(inputname);	
+	printf(" Output file %s\n", outputname.c_str());
+	
+	// point to the input and output files
+	gInputFile = new TFile(inputname.c_str());
+	gOutputFile = new TFile(outputname.c_str(),"RECREATE");
+
+	//get the trees inside
+	gInputFile->ls();
+	
+	gOldTree = (TTree*)gInputFile->Get("MielTree");
+	//gOldTree->SetMakeClass(1); 
+	gOldTree->SetBranchAddress("Miel",&gMiel_st);
+
+	//point to the new tree and set the addresses
+	gNewTree = new TTree("MielDataTree","MielDataTree");
+	gNewTree->Branch("TMielData",&gMielData);
+
+	//Iterate through events
+	Int_t nentries = (Int_t)gOldTree->GetEntries();
+	cout << "Tree contains " << nentries <<endl;
+	
+				
+	for (Int_t i=0 ; i < nentries; i++) {
+		
+		//Get the entry and set the events in the new Tree
+		GoodEvent=false; 
+		//cout << "Event Number : " << i <<endl;
+		//gOldTree->Scan("*","","",1,i);
+		GetEvent(i);	
+
+		for (int j = 0 ; j < 6 ; j++ ) {
+			if (gBuffer_energy.at(j) > 0) { // keep this good event
+				GoodEvent=true; 
+				gMielData->SetMiel(j/*segment*/, gBuffer_energy.at(j), CalibrateEnergy(j,gBuffer_energy.at(j)), gBuffer_time.at(j)) ;
+			}
+		}
+		
+		if (GoodEvent){
+		gMielData->SetAptherix(gBuffer_gamma);
+		gMielData->SetHallProbe(gBuffer_hall);
+		gMielData->SetVcontE(gBuffer_VContE);	
+		gMielData->SetVContG(gBuffer_VContG);
+		
+		//gMielData->Print();
+		gNewTree->Fill();		
+		}
+
+		gMielData->Clear();
+	}
+	
+	// Write the new trees in seperate files 
+	gNewTree->AutoSave();  
+	
+	gOutputFile->Write();
+	gOutputFile->Close();  
+	
+} //end of input files 
+	
+return 0 ; 	
+} 
+
+
+// #########################################################################################
+//                                functions start here 
+// #########################################################################################
+
+void PrintMiel(){
+
+	cout << "E1" << " \t" << "E2" << " \t" << "E3" << " \t" << "E4" << " \t" << "E5" << " \t" << "E6" << " \n";
+	cout << gMiel_st.Miel1E << " \t";
+	cout << gMiel_st.Miel2E << " \t";
+	cout << gMiel_st.Miel3E << " \t";
+	cout << gMiel_st.Miel4E << " \t";
+	cout << gMiel_st.Miel5E << " \t";
+	cout << gMiel_st.Miel6E << " \n";
+	
+	cout << "T1" << " \t" << "T2" << " \t" << "T3" << " \t" << "T4" << " \t" << "T5" << " \t" << "T6" << " \n" ;
+	cout << gMiel_st.Miel1T << " \t";
+	cout << gMiel_st.Miel2T << " \t";
+	cout << gMiel_st.Miel3T << " \t";
+	cout << gMiel_st.Miel4T << " \t";
+	cout << gMiel_st.Miel5T << " \t";
+	cout << gMiel_st.Miel6T << " \n";
+	
+	cout << "gamma" << " \t" << "hall" << " \t" << "VcontE" << " \t" << "VcontG" << " \n" ;
+	cout << gMiel_st.Aptherix << " \t";
+	cout << gMiel_st.HallProbe << " \t";
+	cout << gMiel_st.VcontE << " \t";
+	cout << gMiel_st.VcontG << " \n";
+
+	}
+	
+	
+void PrintBuffers(){
+
+	cout << "======= Print buffers ====  \n" ;
+	cout << "==========================\n" ; 
+
+	cout << "Time size: " << gBuffer_time.size() <<"\n" ;
+			
+	for (unsigned i = 0 ; i < gBuffer_time.size() ; i++ ) {
+	cout << " "<< gBuffer_time.at(i)<<"\t";
+	}
+	cout << "\n-------------------------\n" ; 
+	
+	cout << "Energy size: " << gBuffer_energy.size() <<"\n" ;
+	for (unsigned i = 0 ; i < gBuffer_energy.size() ; i++ ) {
+	cout << " "<< gBuffer_energy.at(i)<<"\t";
+	}
+	cout << "\n-------------------------\n" ; 
+
+	cout << "gBuffer_gamma " << gBuffer_gamma <<" \n";
+		cout << "gBuffer_hall " << gBuffer_hall <<" \n";
+			cout << "gBuffer_VContE " << gBuffer_VContE <<" \n";
+				cout << "gBuffer_VContG " << gBuffer_VContG <<" \n";
+			
+	cout << "==========================\n" ; 
+}
+
+// ##############################
+void ResetBuffers(){
+
+	gBuffer_energy.clear(); 
+	gBuffer_time.clear();
+	gBuffer_gamma =   -1;
+	gBuffer_hall =   -1;
+	gBuffer_VContE =  -1; 
+	gBuffer_VContG =  -1;
+	
+	gBuffer_energy.resize(6,-1); 
+	gBuffer_time.resize(6,-1);
+	
+} 
+
+// ##############################
+void GetEvent(int i){
+
+	ResetBuffers();
+	gOldTree->GetEntry(i);
+	
+    //Fill the buffers 
+		//Miel Energies
+	if(gBuffer_energy.size()==6) {
+		gBuffer_energy.at(0) =   gMiel_st.Miel1E; 
+		gBuffer_energy.at(1) =   gMiel_st.Miel2E; 
+		gBuffer_energy.at(2) =   gMiel_st.Miel3E; 
+		gBuffer_energy.at(3) =   gMiel_st.Miel4E; 
+		gBuffer_energy.at(4) =   gMiel_st.Miel5E; 
+		gBuffer_energy.at(5) =   gMiel_st.Miel6E; 
+	}
+	else{cout << "wrong size" ; exit(-1);}
+	
+		//Miel times
+	if(gBuffer_energy.size()==6) { 
+	gBuffer_time.at(0) =   gMiel_st.Miel1T; 
+	gBuffer_time.at(1) =   gMiel_st.Miel2T; 
+	gBuffer_time.at(2) =   gMiel_st.Miel3T; 
+	gBuffer_time.at(3) =   gMiel_st.Miel4T; 
+	gBuffer_time.at(4) =   gMiel_st.Miel5T; 
+	gBuffer_time.at(5) =   gMiel_st.Miel6T;
+	}
+	else{cout << "wrong size" ; exit(-1);}
+	
+		// gamma energies
+	gBuffer_gamma =   gMiel_st.Aptherix;
+		//Hall probe 
+	gBuffer_hall  =   gMiel_st.HallProbe;
+		//Normalizing parameters
+	gBuffer_VContE =  gMiel_st.VcontE; 
+	gBuffer_VContG =   gMiel_st.VcontG;	
+
+} 
+
+// ##############################
+float CalibrateEnergy(int segment, int E_charge) {
+//need to implement
+return E_charge ; 
+}
+
+// ##############################
+float CalibrateTime(int segment, int T_charge){
+//need to implement 
+return T_charge ; 
+}
+
+
+
